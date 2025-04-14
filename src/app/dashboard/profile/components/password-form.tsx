e054d13b6075918bc82a5a286bcd72e5abed4b75
Omokefe Ovie David <105714886+ovie-daveth@users.noreply.github.com>
2025-04-14 22:40:14 +0100

    fixed bugs on password form

--- src/app/dashboard/profile/components/password-form.tsx
+++ src/app/dashboard/profile/components/password-form.tsx
@@ -28,25 +28,25 @@ export default function PasswordForm() {
     setTimeout(() => {
       setloading(false)
     }, 2000)
-    
+
     console.log("Changing password")
   }
 
   return (
-    <div className="max-w-2xl">
+    <div className="max-w-md">
       <form onSubmit={handleSubmit} className="space-y-6">
       <div className="flex flex-col gap-1">
           <label className="font-normal text-sm" htmlFor="phone">Current Password</label>
-          <input id="current" name="current" type="password" value={passwords.current} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
+          <input id="current" name="current" type="password"  placeholder="Enter your current password" value={passwords.current} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
       </div>
       <div className="flex flex-col gap-1">
           <label className="font-normal text-sm" htmlFor="new">New Password</label>
-          <input id="new" name="new" type="password" value={passwords.new} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
+          <input id="new" name="new" type="password"  placeholder="Enter your new password" value={passwords.new} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
       </div>
 
       <div className="flex flex-col gap-1">
           <label className="font-normal text-sm" htmlFor="confirm">Confirm New Password</label>
-          <input id="confirm" name="confirm" type="password" value={passwords.confirm} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
+          <input id="confirm" name="confirm" type="password"  placeholder="Confirm your new password" value={passwords.confirm} onChange={handleChange} className="border-2 border-gray-300 focus:border-blue-500 focus:ring-blue-500 px-3 py-2.5 rounded-md focus:outline-none font-light text-sm" />
       </div>
 
       <Button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 h-11 text-white rounded-md flex items-center justify-center">
